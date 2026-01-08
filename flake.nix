1e7dc677e96f7da7f3a0b3c61479bb6ab88d136f
trbutler4 <trbiv@proton.me>
2026-01-07 21:30:42 -0600

    flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Go development environment for Advent of Code 2025";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            gopls
+            gotools
+            go-tools
+            delve
+          ];
+
+          shellHook = ''
+            export GOPATH="$PWD/.go"
+            export PATH="$GOPATH/bin:$PATH"
+          '';
+        };
+      }
+    );
+}
